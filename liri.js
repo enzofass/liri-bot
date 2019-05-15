0a4f87c7a97712e8002babcfd8cc4f9a383d92bc
enzofass <zoefassioli@gmail.com>
2019-05-14 21:14:48 -0400

    added function for switch case

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const axios = require("axios");
 const moment = require("moment");
+const fs = require("fs");
 
 const keys = require("./keys.js");
 const Spotify = require("node-spotify-api");
@@ -9,25 +10,34 @@ const spotify = new Spotify(keys.spotify);
 const cmd = process.argv[2];
 const userInput = process.argv.slice(3).join(" ");
 
-switch (cmd) {
-  case "concert-this":
-    console.log("inside concert-this");
-    concertThis(userInput);
-    break;
-  case "spotify-this-song":
-    console.log("inside spotify-this-song");
-    spotifyThis(userInput);
-    break;
-  case "movie-this":
-    console.log("inside movie-this");
-    movieThis(userInput);
-    break;
-  case "do-what-it-says":
-    console.log("inside do-what-it-says");
-    break;
+function goLiri(command, userIn){
+  switch (command) {
+    case "concert-this":
+      console.log("inside concert-this");
+      concertThis(userIn);
+      break;
+    case "spotify-this-song":
+      console.log("inside spotify-this-song");
+      spotifyThis(userIn);
+      break;
+    case "movie-this":
+      console.log("inside movie-this");
+      movieThis(userIn);
+      break;
+    case "do-what-it-says":
+      console.log("inside do-what-it-says");
+      fs.readFile("random.txt", "utf8", function(error, data) {
+        console.log(data);
+        const dataArr = data.split(",");
+        goLiri(dataArr[0], dataArr[1].replace('"','').replace('"',''));
+        console.log(dataArr);
+        console.log("readFile data: ", dataArr[1].replace('"','').replace('"',''));
+        // spotifyThis(dataArr[1]);
+      });
+      break;
+  }
 }
-
-// cmd concertThis -> Band in Town API 
+// cmd concertThis -> Band in Town API
 function concertThis(artist) {
   const queryUrl =
     "https://rest.bandsintown.com/artists/" +
@@ -42,41 +52,38 @@ function concertThis(artist) {
         console.log(`
       =====================
       Venue Name: ${response.data[i].venue.name}
-      Location:   ${response.data[i].venue.city}, ${response.data[i].venue.region}, ${response.data[i].venue.country}
-      Date:       ${moment(response.data[i].venue.datetime).format("MM/DD/YYYY")}
+      Location:   ${response.data[i].venue.city}, ${
+          response.data[i].venue.region
+        }, ${response.data[i].venue.country}
+      Date:       ${moment(response.data[i].venue.datetime).format(
+        "MM/DD/YYYY"
+      )}
       =====================`);
       }
     })
     .catch(function(error) {
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log("---------------Data---------------");
-        console.log(error.response.data);
-        console.log("---------------Status---------------");
-        console.log(error.response.status);
-        console.log("---------------Status---------------");
-        console.log(error.response.headers);
-      } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an object that comes back with details pertaining to the error that occurred.
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log("Error", error.message);
+      if (error) {
+        console.log(`
+        =====================
+        Sorry I could not find that.
+        Error message: ${error}
+        =====================`);
       }
-      console.log(error.config);
     });
 }
 
 function spotifyThis(track) {
   // if user input is blank then default to this
-  if (track == ""){
+  if (track == "") {
     track = "the sign ace of base";
   }
-  spotify.search({ type: 'track', query: track }, function(err, data) {
+  spotify.search({ type: "track", query: track }, function(err, data) {
     if (err) {
-      return console.log('Error occurred: ' + err);
+      return console.log(`
+      =====================
+      Sorry I could not find that.
+      Error message: ${err}
+      =====================`);
     }
     console.log(`
     =====================
@@ -89,18 +96,20 @@ function spotifyThis(track) {
 }
 
 function movieThis(movieName) {
-  if (movieName == ""){
+  if (movieName == "") {
     movieName = "Mr. Nobody";
   }
   // Then run a request with axios to the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+  var queryUrl =
+    "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-// This line is just to help us debug against the actual URL.
-console.log(queryUrl);
+  // This line is just to help us debug against the actual URL.
+  console.log(queryUrl);
 
-axios.get(queryUrl).then(
-  function(response) {
-    console.log(`
+  axios
+    .get(queryUrl)
+    .then(function(response) {
+      console.log(`
       =====================
       Film Title:   ${response.data.Title}
       Year:         ${response.data.Year}
@@ -111,25 +120,15 @@ axios.get(queryUrl).then(
       Plot:         ${response.data.Plot}
       Actors:       ${response.data.Actors}
       =====================`);
-  })
-  .catch(function(error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.log("---------------Data---------------");
-      console.log(error.response.data);
-      console.log("---------------Status---------------");
-      console.log(error.response.status);
-      console.log("---------------Status---------------");
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an object that comes back with details pertaining to the error that occurred.
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log("Error", error.message);
-    }
-    console.log(error.config);
-  });
+    })
+    .catch(function(error) {
+      if (error) {
+        console.log(`
+      =====================
+      Sorry I could not find that.
+      Error message: ${error}
+      =====================`);
+      }
+    });
 }
+goLiri(cmd, userInput);
